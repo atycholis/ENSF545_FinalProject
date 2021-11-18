e4103a71233a82a8e4ca7909f403de3be76007e9
atycholis <attyke@gmail.com>
2021-11-18 09:26:05 -0700

    fixed stalling

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -7,27 +7,27 @@ public class Timer : MonoBehaviour
 {
     public float time = 30.0f;
     public Text displayTime;
-    public GameManager gameManager;
+    //public GameManager gameManager;
 
     // Update is called once per frame
     void Update()
-    {
-        double decimals = System.Math.Round(time, 2);
-
+    { 
         if (time > 0)
         {
             time = time - Time.deltaTime;
-            displayTime.text = decimals.ToString();
 
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                gameManager.EndScene();
-            }
+            //if(Input.GetKeyDown(KeyCode.Space))
+            //{
+            //    gameManager.EndScene();
+            //}
         }
 
-        if(time == 0)
-        {
-            gameManager.EndScene();
-        }
+        double decimals = System.Math.Round(time, 2);
+        displayTime.text = decimals.ToString();
+
+        //if(time == 0)
+        //{
+        //    gameManager.EndScene();
+        //}
     }
 }
